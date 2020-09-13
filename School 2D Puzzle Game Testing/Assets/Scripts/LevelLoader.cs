4a26a2f01da81e99be5ce187901b05a9a6fec162
JurassicJaw <jurassicjaw@gmail.com>
2020-09-13 15:22:11 +1200

    Added Project

---
+++ School 2D Puzzle Game Testing/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+
+    public void loadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Plays the animation
+        transition.SetTrigger("Start");
+
+        //Waits
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load the scene
+        SceneManager.LoadScene(levelIndex);
+
+    }
+
+}
---
+++ School 2D Puzzle Game Testing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+
+    public void PlayGame()
+    {
+        //Loads the next scene in the index. In this case the next scene is level 1.
+        loadNextLevel();
+    }
+
+    public void loadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Plays the animation
+        transition.SetTrigger("Start");
+
+        //Waits
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load the scene
+        SceneManager.LoadScene(levelIndex);
+
+    }
+
+
+
+
+    public void QuitGame()
+    {
+        //quits the application.
+        Application.Quit();
+    }
+
+
+}
