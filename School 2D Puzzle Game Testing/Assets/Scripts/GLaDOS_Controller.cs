8eef6f747fe64352a397d7c3d881428eaf92f4a7
JurassicJaw <jurassicjaw@gmail.com>
2020-09-29 13:57:30 +1300

    Save#16
    Animated GLaDOS, added in an interact button to tun on GLaDOS.
    
    NEEDS SOUND!!

---
+++ School 2D Puzzle Game Testing/Assets/Scripts/GLaDOS_Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GLaDOS_Controller : MonoBehaviour
+{
+
+    public bool isOn;
+    public Animator anim;
+
+    public void TurnedOn()
+    {
+
+        if (!isOn)
+        {
+            isOn = true;
+            Debug.Log("GLaDOS Is Now On");
+            anim.SetBool("isOn", isOn);
+        }
+    }
+
+
+}
---
+++ School 2D Puzzle Game Testing/Assets/Scripts/Interactable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+                Debug.Log("Button Pressed");
+            }
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("In Range");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Out Of Range");
+        }
+    }
+}
