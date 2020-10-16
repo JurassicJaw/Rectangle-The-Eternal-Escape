ff83197a7e12c4748b75107e02d0cd48a47d7b7b
JurassicJaw <jurassicjaw@gmail.com>
2020-10-16 14:05:55 +1300

    Save#19

---
+++ School 2D Puzzle Game Testing/Assets/Scripts/Box/KillBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Crate"))
+            col.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.transform.CompareTag("Crate"))
+            coll.gameObject.SetActive(false);
+    }
+
+}
---
+++ School 2D Puzzle Game Testing/Assets/Scripts/VentDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VentDestroy : MonoBehaviour
+{
+
+    public bool VentGone;
+    public Animator anim;
+
+    public void BrokeVent()
+    {
+
+        if (!VentGone)
+        {
+            VentGone = true;
+            Debug.Log("The vent is now gone.");
+            
+            
+        }
+    }
+
+
+}
