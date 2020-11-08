0308f062d4e6da8e7716a623343713cce2ed1756
JurassicJaw <jurassicjaw@gmail.com>
2020-11-09 09:42:10 +1300

    Save#25
    - Envoriment Textures Added
    - Finished Game
    
    ~ Needs Ending
    ~ Needs Better Menu

--- School 2D Puzzle Game Testing/Assets/Scripts/Box/KillBox.cs
+++ School 2D Puzzle Game Testing/Assets/Scripts/Box/KillBox.cs
@@ -5,29 +5,35 @@ using UnityEngine;
 public class KillBox : MonoBehaviour
 {
 
-    public Transform boxSpawn;
-    public Transform crate;
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.transform.CompareTag("Crate"))
+<<<<<<< HEAD
         {
             col.transform.position = boxSpawn.position;
             crate.GetComponent<Rigidbody2D>().gravityScale = 0;
         }
 
         
+=======
+            col.gameObject.SetActive(false);
+>>>>>>> parent of 8f07422... Save#24
     }
 
     private void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.transform.CompareTag("Crate"))
+<<<<<<< HEAD
         {
             coll.transform.position = boxSpawn.position;
             crate.GetComponent<Rigidbody2D>().gravityScale = 0;
         }
 
          
+=======
+            coll.gameObject.SetActive(false);
+>>>>>>> parent of 8f07422... Save#24
     }
 
 }
