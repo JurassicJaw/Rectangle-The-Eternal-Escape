af183931731eb27b3fd4f89d2ab1f405e6de4da9
JurassicJaw <jurassicjaw@gmail.com>
2020-11-11 14:05:51 +1300

    Save#28
    -Finished Main Menu
    
    *Needs Title

---
+++ School 2D Puzzle Game Testing/Assets/Scripts/Menu/MenuPortal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPortal : MonoBehaviour
+{
+
+    public Transform portalExit;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Player"))
+
+        {
+            col.transform.position = portalExit.position;
+        }
+
+        if (col.transform.CompareTag("Crate"))
+
+        {
+            col.transform.position = portalExit.position;
+        }
+
+
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.transform.CompareTag("Player"))
+
+        {
+            coll.transform.position = portalExit.position;
+        }
+
+        if (coll.transform.CompareTag("Crate"))
+
+        {
+            coll.transform.position = portalExit.position;
+        }
+
+    }
+
+}
